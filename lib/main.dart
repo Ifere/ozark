f3b5fa9a68743b2461ab786f171a24c06ca2227c
Ifere <ifere1066@gmail.com>
2020-05-31 10:14:26 +0100

    ore

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,10 @@ import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 
+import 'screens/login_screen.dart';
+import 'screens/registration_screen.dart';
+import 'screens/welcome_screen.dart';
+
 void main() => runApp(FlashChat());
 
 class FlashChat extends StatelessWidget {
@@ -15,6 +19,14 @@ class FlashChat extends StatelessWidget {
           body1: TextStyle(color: Colors.black54),
         ),
       ),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => WelcomeScreen(),
+        RegistrationScreen.id: (context) => WelcomeScreen(),
+        ChatScreen.id: (context) => WelcomeScreen(),
+
+      },
       home: WelcomeScreen(),
     );
   }
